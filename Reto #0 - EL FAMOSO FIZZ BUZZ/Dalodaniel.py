1000dc93187133d292aa0bd7f9b6b28e7d515c46
Dany Montero <dalo_daniel@hotmail.com>
2023-12-13 18:47:42 +0100

    Cambio de ruta

---
+++ Reto #0 - EL FAMOSO FIZZ BUZZ/Dalodaniel.py
@@ -0,0 +1,25 @@
+'''
+ Escribe un programa que muestre por consola (con un print) los
+ * números de 1 a 100 (ambos incluidos y con un salto de línea entre
+ * cada impresión), sustituyendo los siguientes:
+ * - Múltiplos de 3 por la palabra "fizz".
+ * - Múltiplos de 5 por la palabra "buzz".
+ * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+ */'''
+
+fizzbuzz = []
+
+for i in range(101):
+    if i % 3 == 0 and i % 5 == 0:
+        fizzbuzz.append("fizzbuzz")
+    
+    elif i % 3 == 0:
+        fizzbuzz.append("fizz")
+    
+    elif i % 5 == 0:
+        fizzbuzz.append("buzz")
+    
+    else:
+        fizzbuzz.append(i)
+
+print(fizzbuzz)
